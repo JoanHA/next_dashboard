968f8a45c9581a3789ac2ef0f41feaa783f8c231
JoanHA <joanhurtado134@outlook.es>
2024-03-29 15:53:18 -0500

    Done

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons'
+function loading() {
+  return (
+    <div><DashboardSkeleton></DashboardSkeleton></div>
+  )
+}
+
+export default loading
